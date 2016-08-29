199ceb9901dae643f67ac078fdaf25b609569e88
RasCarlito <rascarlito@gmail.com>
2016-08-29 16:55:03 +0200

    test(regex): Add tests for regex method

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 import test from 'ava'
-import url from '../dist/url-composer.min'
+import url from '../dist/url-composer'
 
 test('url.build - Should build a complete URL according to passed options', t => {
   const options = {
@@ -75,4 +75,15 @@ test('url.query - Should build a query string from given parameters', t => {
   t.is(query, 'a=1&b=2&c=3')
   t.is(url.query({ query: {} }), '')
   t.is(url.query(), '')
+})
+
+test('url.regex - Should generate a regular expression from a path', t => {
+  t.is(url.regex('/users/:id').test('/users/42'), true)
+})
+
+test('url.test - Should transform a path to regex and test a given url against it', t => {
+  t.is(url.test({ path: '/users/:id', url: '/users/42' }), true)
+  t.is(url.test({ path: '/users/:id(/edit/:section)', url: '/users/42' }), true)
+  t.is(url.test({ path: '/users/:id(/edit/:section)', url: '/users/1234/edit/stuff' }), true)
+  t.is(url.test({ path: '/users/:id(/edit/:section)', url: '/non/matching/url' }), false)
 })
